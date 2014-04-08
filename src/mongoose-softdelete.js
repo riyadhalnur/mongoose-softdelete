3532e1fd9cba4929ca1a2719e3c81da2b2577479
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2014-04-08 10:42:20 +0600

    change in field name for deleted by user

--- src/mongoose-softdelete.js
+++ src/mongoose-softdelete.js
@@ -1,17 +1,17 @@
 var exports;
 
 exports = module.exports = function(schema) {
-  schema.add({ deleted: Boolean, deleted_by: String });
+  schema.add({ deleted: Boolean, deletedBy: String });
 
   schema.methods.softdelete = function(data, callback) {
     this.deleted = true;
-    this.deleted_by = data._id;
+    this.deletedBy = data._id;
     this.save(callback);
   };
 
   schema.methods.restore = function(data, callback) {
     this.deleted = false;
-    this.deleted_by = null;
+    this.deletedBy = null;
     this.save(callback);
   };
 };
