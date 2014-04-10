1b8c1da2fea1458137c2f69c02762161054f6d55
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2014-04-10 10:04:17 +0600

    version bump to 0.2.3. now adds deleted field with pre save hook

--- src/mongoose-softdelete.js
+++ src/mongoose-softdelete.js
@@ -3,6 +3,13 @@ var exports;
 exports = module.exports = function(schema) {
   schema.add({ deleted: Boolean, deletedBy: String });
 
+  schema.pre('save', function (next) {
+    if (!this.deleted) {
+      this.deleted = false;
+    }
+    next();
+  });
+
   schema.methods.softdelete = function(data, callback) {
     this.deleted = true;
     this.deletedBy = data._id;
