06acf477a3121e96a3064d5b918dcaa441875e82
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2014-04-13 11:14:47 +0600

    version 0.2.4.user data is now optional

--- src/mongoose-softdelete.js
+++ src/mongoose-softdelete.js
@@ -12,13 +12,17 @@ exports = module.exports = function(schema) {
 
   schema.methods.softdelete = function(data, callback) {
     this.deleted = true;
-    this.deletedBy = data._id;
+    if (data) {
+      this.deletedBy = data._id;
+    }
     this.save(callback);
   };
 
   schema.methods.restore = function(data, callback) {
     this.deleted = false;
-    this.deletedBy = null;
+    if (data) {
+      this.deletedBy = null;
+    }
     this.save(callback);
   };
 };
